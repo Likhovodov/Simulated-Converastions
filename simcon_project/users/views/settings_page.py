2644eb33e929c6dff8f8a76c812f6a9119263836
Jeremy Carnahan <jcjerkie@gmail.com>
2021-01-17 17:02:55 -0800

    All of SYMCO-11, Django has a standard form for changing passwords

--- simcon_project/users/views/settings_page.py
+++ simcon_project/users/views/settings_page.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
 
 
 @login_required
 def SettingsView(request):
-    return render(request, 'settings_view.html')
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'settings_view.html', {
+        'form': form
+    })
